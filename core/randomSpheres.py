1f33189866a61d4b1d020c6e1569488d7be2682d
William Laderer <wtladerer@gmail.com>
2022-09-29 19:53:22 -0700

    added more documentation

--- core/randomSpheres.py
+++ core/randomSpheres.py
@@ -4,9 +4,9 @@ import numpy as np
 
 def rotation_matrix_from_vectors(vec1, vec2):
     """ Find the rotation matrix that aligns vec1 to vec2
-    :param vec1: A 3d "source" vector -- this would be our thf axis
-    :param vec2: A 3d "destination" vector --- this would be the negative of the Yb - O bond
-    :return mat: A transform matrix (3x3) which when applied to vec1, aligns it with vec2.
+    vec1 is the ligand axis
+    vec2 is the negative of the Yb - O bond
+    returns a transform matrix (3x3) which when applied to vec1, aligns it with vec2.
     """
     a, b = (vec1 / np.linalg.norm(vec1)).reshape(3), (vec2 / np.linalg.norm(vec2)).reshape(3)
     v = np.cross(a, b)
@@ -18,26 +18,29 @@ def rotation_matrix_from_vectors(vec1, vec2):
 
 
 def fibonacci_sphere(samples=100):
-
+    """
+    Generates a sphere around the coordination complex
+    Number of points is adjustable
+    """
     points = []
     phi = np.pi * (3. - np.sqrt(5.))  # golden angle in radians
 
     for i in range(samples):
         y = 1 - (i / float(samples - 1)) * 2  # y goes from 1 to -1
         radius = np.sqrt(1 - y * y)  # radius at y
-
         theta = phi * i  # golden angle increment
 
         x = np.cos(theta) * radius
         z = np.sin(theta) * radius
-
         points.append((x, y, z))
 
     return points
 
 def writeLines(lines, filename="xyzs.xyz"):
+    """
+    Writes generated strings to an xyz file
+    """
     n_lines = len(lines)
-
     with open(filename, 'w') as f:
         
         f.write(f"{n_lines}\n\n") #create header for xyz file
@@ -47,6 +50,9 @@ def writeLines(lines, filename="xyzs.xyz"):
 
 
 def validPoints(sphere, xyzarr, cutoff=1.5):
+    """
+    Tests if a point within the generated sphere is too close to a atom within a designated cutoff distance
+    """
     points = []
     for point in sphere:
         dists = []
@@ -64,25 +70,36 @@ def validPoints(sphere, xyzarr, cutoff=1.5):
     return points
 
 def set_origin(xyzarr):
-
+    """
+    Sets the first atom in the xyz array as the origin
+    It is recommended that you format your xyz file such that the coordination complex is the first atom
+    """
     xyzarr = xyzarr - xyzarr[0]
     return xyzarr
 
 
 def getInfo(file):
+    """
+    Primes the xyzarr and sets the origin for other vector caclulus needs
+    """
     xyzarr, atoms = readxyz(file) 
     xyzarr = set_origin(xyzarr)
 
     return xyzarr, atoms
 
 def sphericalSample(radius, xyzarr):
+    """
+    Returns the points on the sample sphere of a given radius
+    """
     sphere = radius*np.array(fibonacci_sphere())
     points = validPoints(sphere, xyzarr)
 
     return points
 
-def makeLigand(file):
-    file = "/home/wladerer/github/minis/thf.xyz"
+def makeLigand(file = "/home/wladerer/github/minis/thf.xyz"):
+    """
+    Currently only works for THF - missing a way to define the axis of the ligand needed for rotation
+    """
     ligand_arr, ligand_atoms = getInfo(file)
     ligand_axis = ligand_arr[0] - ((ligand_arr[1] + ligand_arr[2])/2)
 
