5b6da3ec2735fd7911643c0f98f14b5de3062bf7
William Laderer <wtladerer@gmail.com>
2022-09-30 18:45:10 -0700

    testing autoDFT

--- core/autoDFT.py
+++ core/autoDFT.py
@@ -3,14 +3,14 @@ import yaml
 from turbomoleio.input.define import DefineRunner
 import os
 
-def define(file, parameter_file):
+def define():
     """
         Takes xyz file and parameter.yaml file and creates all requisite files for TURBOMOLE calculation
     """
     
-    os.system(f"x2t {file} > coord")
+    # os.system(f"x2t {file} > coord")
 
-    with open(parameter_file, "r") as fh:
+    with open("/home/wladerer/github/Coordinate/utils/parameters.yaml", "r") as fh:
         dp = yaml.load(fh, Loader=yaml.SafeLoader)
         dr = DefineRunner(parameters=dp)
         dr.run_full()
@@ -21,3 +21,4 @@ def fixturbo():
     """
     os.system(f'sed -i "s/scforbitalshift  closedshell=.05/scforbitalshift  closedshell=.3 /" control')
 
+define()
---
+++ core/prepareFiles.py
@@ -0,0 +1,16 @@
+from autoDFT import *
+
+file3 = "/home/wladerer/github/Coordinate/core/damp5_converged/file3_damp5_coord"
+file15 = "/home/wladerer/github/Coordinate/core/damp5_converged/file15_damp5_coord"
+file18 = "/home/wladerer/github/Coordinate/core/damp5_converged/file18_damp5_coord"
+file26 = "/home/wladerer/github/Coordinate/core/damp5_converged/file26_damp5_coord"
+files = [file3, file15, file18, file26]
+
+names= ["file3", "file15", "file18", "file26"]
+
+for name, file in zip(names, files):
+    os.system(f"mkdir {name}")
+    os.system(f"cd {name}")
+    os.system(f"cp {file} coord")
+    define("/home/wladerer/github/Coordinate/utils/parameters.yaml")
+    os.system("cd ..")
