11fb3bd757f008972d57d2fac9d838a51bec928a
William <wtladerer@gmail.com>
2022-10-02 16:02:15 -0700

    making autoDFT more generalized

--- Coordinate/autoDFT.py
+++ Coordinate/autoDFT.py
@@ -3,13 +3,22 @@ import yaml
 from turbomoleio.input.define import DefineRunner
 import os
 
-def define():
+def define(yaml_file: str ="parameters.yaml", convert_xyz: bool=False, xyz_file: str = "*.xyz"):
     """
         Takes coord file and parameter.yaml file and creates all requisite files for TURBOMOLE calculation
-    """
-    
 
-    with open("parameters.yaml", "r") as fh:
+        yaml_file: filename for the yaml file that contains the user's specified TURBOMOLE parameters
+        convert_xyz: a boolean to specify if there is an existing xyz file to be converted to TURBOMOLE coord format
+        xyz_file: chooses the xyz file present in the current directory as default, but can be changed to an alternative file
+
+        Output: alpha, basis, auxbasis, control, coord (iff convert_xyz == True) 
+    """
+
+    if convert_xyz: 
+        
+        os.system(f"x2t {xyz_file} > coord")
+
+    with open(yaml_file, "r") as fh:
         dp = yaml.load(fh, Loader=yaml.SafeLoader)
         dr = DefineRunner(parameters=dp)
         dr.run_full()
